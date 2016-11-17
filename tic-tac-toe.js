458d7017d8461cd0acec98352dc1faf4029619e1
dustin wagner <dwmodman@icloud.com>
2016-11-17 14:03:05 -0800

    did stuff

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -4,3 +4,12 @@ var namespace = "http://www.w3.org/2000/svg"
 function makeshape() {
 //code to create a shape
 }
+function redRect(){
+  var circle = document.createElementNS(namespace,"circle")
+  circle.setAtrribute("cx",5)
+  circle.setAtrribute("cy",30)
+  circle.setAtrribute("r",15)
+  circle.setAtrribute("fill","black")
+var canvas = document.getElementById("game-borad")
+canvas.appendChild(circle)
+}
