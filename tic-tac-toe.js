d0f2d621a6874f857baca5da30dc231c0013429d
dustin wagner <dwmodman@icloud.com>
2016-11-28 14:21:12 -0800

    done ig

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -10,7 +10,7 @@ function redRect(){
   circle.setAttribute("cy",30)
   circle.setAttribute("r",15)
   circle.setAttribute("fill","black")
-var canvas = document.getElementById("game-borad")
+var canvas = document.getElementById("game-board")
 canvas.appendChild(circle)
 }
 function orangeRect(){
@@ -37,5 +37,51 @@ circle.setAtrribute("cx",5)
 circle.setAtrribute("cy",95)
 circle.setAtrribute("r",15)
 circle.setAtrribute("fill","black")
-var canvas = document.getElementById()
+var canvas = document.getElementById("game-borad")
+canvas.appendChild(circle)
+}
+function blueRect() {
+var circle = document.createElementNS(namespace,"circle")
+circle.setAtrribute("cx",70)
+circle.setAtrribute("cy",95)
+circle.setAtrribute("r",15)
+circle.setAtrribute("fill","black")
+var canvas = document.getElementById("game-borad")
+canvas.appendChild(circle)
+}
+function purpleRect() {
+var circle = document.createElementNS(namespace,"circle")
+circle.setAtrribute("cx",135)
+circle.setAtrribute("cy",95)
+circle.setAtrribute("r",15)
+circle.setAtrribute("fill","black")
+var canvas = document.getElementById("game-borad")
+canvas.appendChild(circle)
+}
+function pinkRect() {
+var circle = document.createElementNS(namespace,"circle")
+circle.setAtrribute("cx",5)
+circle.setAtrribute("cy",95)
+circle.setAtrribute("r",15)
+circle.setAtrribute("fill","black")
+var canvas = document.getElementById("game-borad")
+canvas.appendChild(circle)
+}
+function fuchsiaRect() {
+var circle = document.createElementNS(namespace,"circle")
+circle.setAtrribute("cx",5)
+circle.setAtrribute("cy",95)
+circle.setAtrribute("r",15)
+circle.setAtrribute("fill","black")
+var canvas = document.getElementById("game-borad")
+canvas.appendChild(circle)
+}
+function brownRect() {
+var circle = document.createElementNS(namespace,"circle")
+circle.setAtrribute("cx",5)
+circle.setAtrribute("cy",95)
+circle.setAtrribute("r",15)
+circle.setAtrribute("fill","black")
+var canvas = document.getElementById("game-borad")
+canvas.appendChild(circle)
 }
